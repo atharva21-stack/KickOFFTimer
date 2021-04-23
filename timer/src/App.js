991e89d2fcd88101d8c6edf1901a686a8bdf4b34
Atharva Chandwadkar <chandwadkar28@gmail.com>
2021-04-23 23:30:10 +0530

    Added App js & css

---
+++ timer/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import "./App.css"
+
+
+function App() {
+        
+        
+        return (
+    
+        <section className="timer-container">
+        <section className="timer">
+        <div>
+        
+        <h2>Countdown Timer</h2>
+              <p>Countdown to really special date</p>
+        </div>
+        <div>
+          <section>
+                  <p>87</p>
+                  <p><small>Days</small></p>
+          </section>
+          <span>:</span>
+
+
+          <section>
+                  <p>87</p>
+                  <p><small>Hours</small></p>
+          </section>
+          <span>:</span>
+
+          <section>
+                  <p>87</p>
+                  <p><small>Minutes</small></p>
+          </section>
+          <span>:</span>
+          <section>
+                  <p>87</p>
+                  <p><small>Secs</small></p>
+          </section>
+          
+        </div>
+        
+          </section>
+        </section>
+      
+  )
+}
+
+export default App
